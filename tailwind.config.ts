ca00558bef38ac05275e9b5cf7ee91d8e9ec71ea
ProgramadorGod <pipenet12@gmail.com>
2026-04-13 17:31:18 -0500

    working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
+
 
 export default {
   darkMode: ["class"],
@@ -89,5 +91,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 } satisfies Config;
